f29a87b79adc1dc539add748d806998b07f3368b
Omar Andres Gutierrez Daza <omarandresx2@hotmail.com>
2025-04-21 23:20:51 -0500

    INICIO cambios formales - Logica inicial login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using bufinscustomers.Permisos;
 
 namespace bufinscustomers.Controllers
 {
+    [ValidarSesion]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult CerrarSesion()
+        {
+            Session["usuario"] = null;
+            return RedirectToAction("Login", "Acceso");
         }
     }
 }
---
+++ Models/Usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bufinscustomers.Models
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public string ConfirmarClave { get; set; }
+    }
+}
